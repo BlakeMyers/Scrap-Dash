a39d11e3cf20b414231a76662b1c7beca324429c
Blake Myers <blaketoyo@live.com>
2020-04-20 22:25:19 -0700

    Simple Timer HUD + Pause
    Created HUD concept image and created a simple controller script that times time not spent paused.

---
+++ Assets/UI/Scripts/UI_Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject HudPanel;
+    public GameObject PausePanel;
+    public Text TimeText;
+    public Text GoalText;
+    bool pausetime = false;
+    float time = 0.0f;
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Pause();
+        }
+        if (!pausetime) {
+            time += Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        TimeText.text = "Time: \n" + time.ToString("0.0");
+    }
+
+    private void Pause()
+    {
+        if (!pausetime) {
+            pausetime = true;
+            PausePanel.SetActive(true);
+        }
+    }
+
+    public void Resume() {
+        PausePanel.SetActive(false);
+        pausetime = false;
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = (false);
+#else
+        Application.Quit();
+#endif   
+    }
+}
