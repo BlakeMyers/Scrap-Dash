3f0d9f32ac3152a67a45a51f5b462dc2aa2fbe3b
Garrison Price <garrison.b.price@gmail.com>
2020-05-05 20:26:53 -0700

    Switched terrain generator to mine because I know it better. Added code for randomly placing buildings, needs fixes.

---
+++ Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,82 @@
+﻿using PGC_Terrain;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour
+{
+    public PGCTerrain pgcTerrain;
+    public AnimationCurve buildingHeightDistribution;
+    public AnimationCurve buildingFootprintSizeDistribution;
+    [Range(10,50)]
+    public int numberOfBuildings = 20;
+    [Range(5,20)]
+    public float maxBuildingHeight = 10;
+    [Range(1,5)]
+    public float minBuildingHeight = 3;
+    [Range(8,20)]
+    public float maxBuildingFootprint = 15;
+    [Range(3, 8)]
+    public float minBuildingFootprint = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRandomTerrain();
+        BuildWalls();
+        PlaceBuildings();
+    }
+
+    private void GenerateRandomTerrain()
+    {
+        pgcTerrain.perlinScale = 93;
+        pgcTerrain.perlinFrequency = 5;
+        pgcTerrain.perlinAmplitude = 0.01f;
+
+        pgcTerrain.ApplyPerlinNoise(pgcTerrain.gameObject.GetComponent<Terrain>(), false);
+    }
+
+    // Build walls around the outside of the map
+    private void BuildWalls()
+    {
+
+    }
+
+    private void PlaceBuildings()
+    {
+        int frequencyResolution = 100;
+        List<float> heightFrequencyDistribution = new List<float>();
+        List<float> footprintFrequencyDistribution = new List<float>();
+        for (int curvePositionIndex = 0; curvePositionIndex < frequencyResolution; curvePositionIndex++)
+        {
+            float step = (float)curvePositionIndex / (float)frequencyResolution;
+            int heightFrequency = (int)(Mathf.Clamp01(buildingHeightDistribution.Evaluate(step)) * frequencyResolution);
+            float heightRange = maxBuildingHeight - minBuildingHeight;
+            for(int i = 0; i < heightFrequency; i++)
+            {
+                heightFrequencyDistribution.Add(step * heightRange + minBuildingHeight);
+            }
+
+
+            int footprintFrequency = (int)(Mathf.Clamp01(buildingFootprintSizeDistribution.Evaluate(step)) * frequencyResolution);
+            float footprintRange = maxBuildingFootprint - minBuildingFootprint;
+            for (int i = 0; i < heightFrequency; i++)
+            {
+                footprintFrequencyDistribution.Add(step * footprintRange + minBuildingFootprint);
+            }
+        }
+
+        for(int i = 0; i < numberOfBuildings; i++)
+        {
+            float buildingHeight = heightFrequencyDistribution[Random.Range(0, heightFrequencyDistribution.Count)];
+            float buildingFootprint = footprintFrequencyDistribution[Random.Range(0, footprintFrequencyDistribution.Count)];
+
+            Vector3 buildingPosition = Random.insideUnitSphere * pgcTerrain.gameObject.GetComponent<Terrain>().terrainData.size.x / 2 + new Vector3(pgcTerrain.gameObject.GetComponent<Terrain>().terrainData.size.x, 0, pgcTerrain.gameObject.GetComponent<Terrain>().terrainData.size.x) / 2;
+            buildingPosition.y = pgcTerrain.gameObject.GetComponent<Terrain>().SampleHeight(buildingPosition);
+
+            GameObject building = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            building.transform.position = buildingPosition;
+            building.transform.localScale = new Vector3(buildingFootprint, buildingHeight * 2, buildingFootprint);
+            building.transform.rotation = Quaternion.Euler(Random.Range(-7f, 7f), Random.Range(-45f, 45f), Random.Range(-7f, 7f));
+        }
+    }
+}
--- Assets/Scripts/Terrain/PGCTerrain.cs
+++ Assets/Scripts/Terrain/PGCTerrain.cs
@@ -1,108 +1,204 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Linq;
 using UnityEngine;
-using UnityEditor;
 
-
-public enum Generator{
-    Random,
-    SinglePerlin,
-    MuliplePerlin
-}
-
-[ExecuteInEditMode]
-public class PGCTerrain : MonoBehaviour
+namespace PGC_Terrain
 {
-    public Generator generator;
+    public class PGCTerrain : MonoBehaviour
+    {
+        public float perlinScale = 1;
+        public float perlinFrequency = 1;
+        public float perlinAmplitude = 1;
 
-    TerrainData terrainData;
-    public float weight;
+        public void ApplyPerlinNoise(Terrain terrain, bool additive = false)
+        {
+            float[,] perlinNoise = Noise.GeneratePerlinNoise(terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution, perlinScale, perlinFrequency, perlinAmplitude);
 
-    public int octaves = 1;
-    public float multiplePerlinWeight = 1;
-
-    void OnEnable(){
-        this.terrainData = Terrain.activeTerrain.terrainData;
-
-    }
-
-    public void GenerateTerrain(float weight, Generator generator){
-        switch(generator){
-            case Generator.Random:
-                RandomGeneration(weight);
-            break;
-            
-            case Generator.SinglePerlin:
-                SPerlinGeneration(weight);
-            break;
-            
-            case Generator.MuliplePerlin:
-                MPerlinGeneration(weight);
-            break;
-        }
-    }
-
-    public void ResetTerrain(){
-        int size = terrainData.heightmapResolution;
-        float[,] heights = new float[size,size];
-        
-        terrainData.SetHeights(0,0, heights);
-    }
-
-    public void RandomGeneration(float weight){
-        float[,] heights = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
-        int w = terrainData.heightmapResolution;
-        int h = w;
-        
-        for(int i = 0; i < w; i++){
-            for(int j = 0; j < h; j++){
-                heights[i, j] += Random.Range(0f, 1f) * weight;
-            }
+            ApplyTerrainHeightmap(terrain, perlinNoise, additive);
         }
 
-        terrainData.SetHeights(0,0, heights);
-    }
+        public int perlinNumberOfOctaves = 4;
+        public float perlinPersistence;
+        public float perlinLacunarity;
 
-    public void SPerlinGeneration(float weight){
-        float[,] heights = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
-        int w = terrainData.heightmapResolution;
-        int h = w;
-        float xpos = Random.Range(0f, terrainData.heightmapResolution);
-        float ypos = Random.Range(0f, terrainData.heightmapResolution);
-        
-        for(int i = 0; i < w; i++){
-            for(int j = 0; j < h; j++){
-                float height = Mathf.PerlinNoise(
-                    xpos + (float)i/(float)w,
-                    ypos + (float)j/(float)h);
+        public void ApplyMultiplePerlinNoise(Terrain terrain, bool additive = false)
+        {
+            float[,] perlinNoise = Noise.GenerateMultiplePerlinNoise(terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution, perlinScale, perlinNumberOfOctaves, perlinPersistence, perlinLacunarity);
 
-                heights[i, j] += height * weight;
-            }
+            ApplyTerrainHeightmap(terrain, perlinNoise, additive);
         }
 
-        terrainData.SetHeights(0,0, heights);
-    }
+        public float voronoiMountainRadius;
+        public float voronoiMountainHeight;
+        public float voronoiMountainCurvature;
 
-    public void MPerlinGeneration(float weight){
-        float[,] heights = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
-        int w = terrainData.heightmapResolution;
-        int h = w;
-        float[] randPos = new float[octaves * 2];
-        for(int i = 0; i < randPos.Length; i++){
-            randPos[i] = Random.Range(0f, terrainData.heightmapResolution);
+        public void ApplyVornoiMountain(Terrain terrain, bool additive = false)
+        {
+            float[,] voronoiMountain = Noise.GenerateVoronoiMountain(terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution, voronoiMountainRadius, voronoiMountainHeight, voronoiMountainCurvature);
+
+            ApplyTerrainHeightmap(terrain, voronoiMountain, additive);
         }
 
-        for(int i = 0; i < w; i++){
-            for(int j = 0; j < h; j++){
-                float height = 0;
-                for(int k = 0; k < octaves; k++){
-                    height += Mathf.PerlinNoise(
-                                randPos[k]   + ((float)i/(float)w * (float)Mathf.Pow(2, k)),
-                                randPos[k+1] + ((float)j/(float)h* (float)Mathf.Pow(2, k))) * (1/Mathf.Pow(2, k));
+        public float mpdStartingSpread;
+        public float mpdSpreadReductionConstant;
+
+        public void ApplyMPDNoise(Terrain terrain, bool additive = false)
+        {
+            float[,] mpdNoise = Noise.GenerateMidpointDisplacement(terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution, mpdStartingSpread, mpdSpreadReductionConstant, additive ? terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution) : null);
+
+            ApplyTerrainHeightmap(terrain, mpdNoise, false);
+        }
+
+        private void ApplyTerrainHeightmap(Terrain terrain, float[,] heightMap, bool additive = false)
+        {
+            float[,] currentTerrainHeightmap;
+            if (additive)
+            {
+                currentTerrainHeightmap = GetCurrentHeightmap(terrain);
+
+                for (int x = 0; x < Mathf.Sqrt(heightMap.Length); x++)
+                {
+                    for (int y = 0; y < Mathf.Sqrt(heightMap.Length); y++)
+                    {
+                        currentTerrainHeightmap[x, y] += heightMap[x, y];
+                    }
                 }
-                heights[i, j] += height * weight;
+
+                NormalizeFloatArray(currentTerrainHeightmap, 0, 1);
             }
+            else
+            {
+                currentTerrainHeightmap = heightMap;
+            }
+
+            terrain.terrainData.SetHeights(0, 0, currentTerrainHeightmap);
         }
-        terrainData.SetHeights(0,0, heights);
+
+        private const int DIRT = 0;
+        private const int GRASS = 1;
+        private const int ROCK = 2;
+        private const int SNOW = 3;
+
+        public float minHeightDirt;
+        public float maxHeightDirt;
+        public float minSlopeDirt;
+        public float maxSlopeDirt;
+
+        public float minHeightGrass;
+        public float maxHeightGrass;
+        public float minSlopeGrass;
+        public float maxSlopeGrass;
+
+        public float minHeightRock;
+        public float maxHeightRock;
+        public float minSlopeRock;
+        public float maxSlopeRock;
+
+        public float minHeightSnow;
+        public float maxHeightSnow;
+        public float minSlopeSnow;
+        public float maxSlopeSnow;
+
+        public void ApplyTerrainTextures(Terrain terrain)
+        {
+            int alphamapWidth = terrain.terrainData.alphamapWidth;
+            int alphamapHeight = terrain.terrainData.alphamapHeight;
+            int heightmapWidth = terrain.terrainData.heightmapResolution;
+            int heightmapHeight = terrain.terrainData.heightmapResolution;
+
+            // Output splatmap data
+            float[,,] splatmap = new float[alphamapWidth, alphamapHeight, 4];
+
+            for (int x = 0; x < alphamapWidth; x++)
+            {
+                for (int y = 0; y < alphamapHeight; y++)
+                {
+                    // x and y sample between 0 and 1
+                    // They are swapped for some reason...
+                    float ySample = x/((float)alphamapWidth);
+                    float xSample = y/((float)alphamapHeight);
+
+                    float height = terrain.terrainData.GetHeight((int)(xSample * heightmapWidth), (int)(ySample * heightmapHeight)) / terrain.terrainData.size.y;
+                    float slope = terrain.terrainData.GetSteepness(xSample, ySample);
+
+                    float[] weights = new float[4];
+
+                    float dirtHeightInput = maxHeightDirt >= height && minHeightDirt <= height ? (Mathf.Sin(Mathf.PI * height / (maxHeightDirt - minHeightDirt)) + 1) / 2 : 0;
+                    float grassHeightInput = maxHeightGrass >= height && minHeightGrass <= height ? (Mathf.Sin(Mathf.PI * height / (maxHeightGrass - minHeightGrass)) + 1) / 2 : 0;
+                    float rockHeightInput = maxHeightRock >= height && minHeightRock <= height ? (Mathf.Sin(Mathf.PI * height / (maxHeightRock - minHeightRock)) + 1) / 2 : 0;
+                    float snowHeightInput = maxHeightSnow >= height && minHeightSnow <= height ? (Mathf.Sin(Mathf.PI * height / (maxHeightSnow - minHeightSnow)) + 1) / 2 : 0;
+
+                    float dirtSlopeInput = maxSlopeDirt >= slope && minSlopeDirt <= slope ? (Mathf.Sin(Mathf.PI * slope / (maxSlopeDirt - minSlopeDirt)) + 1) / 2 : 0;
+                    float grassSlopeInput = maxSlopeGrass >= slope && minSlopeGrass <= slope ? (Mathf.Sin(Mathf.PI * slope / (maxSlopeGrass - minSlopeGrass)) + 1) / 2 : 0;
+                    float rockSlopeInput = maxSlopeRock >= slope && minSlopeRock <= slope ? (Mathf.Sin(Mathf.PI * slope / (maxSlopeRock - minSlopeRock)) + 1) / 2 : 0;
+                    float snowSlopeInput = maxSlopeSnow >= slope && minSlopeSnow <= slope ? (Mathf.Sin(Mathf.PI * slope / (maxSlopeSnow - minSlopeSnow)) + 1) / 2 : 0;
+
+
+                    weights[DIRT] = dirtHeightInput + dirtSlopeInput;
+                    weights[GRASS] = grassHeightInput + grassSlopeInput;
+                    weights[ROCK] = rockHeightInput + rockSlopeInput;
+                    weights[SNOW] = snowHeightInput + snowSlopeInput;
+
+                    float weightsTotal = weights.Sum();
+
+                    // Make sum of all weights 1 and set output splatmap
+                    for (int i = 0; i < 4; i++)
+                    {
+                        weights[i] /= weightsTotal;
+                        splatmap[x, y, i] = weights[i];
+                    }
+                }
+            }
+
+            // Set texture splatmap
+            terrain.terrainData.SetAlphamaps(0, 0, splatmap);
+        }
+
+        //Min Max normalization
+        private float[,] NormalizeFloatArray(float[,] floatArray, float min, float max)
+        {
+            for (int i = 0; i < floatArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < floatArray.GetLength(1); j++)
+                {
+                    floatArray[i, j] = (floatArray[i, j] - min) / (max - min);
+                }
+            }
+
+            return floatArray;
+        }
+
+        private float[,] GetEmptyHeightmap(Terrain terrain)
+        {
+            return new float[terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution];
+        }
+
+        private float[,] GetCurrentHeightmap(Terrain terrain)
+        {
+            return terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution);
+        }
+
+        private Tuple<float,float> GetMinMax(float [,] floatArray)
+        {
+            float min = float.MaxValue;
+            float max = float.MinValue;
+            
+            for(int i = 0; i < floatArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < floatArray.GetLength(1); j++)
+                {
+                    if (floatArray[i, j] > max)
+                    {
+                        max = floatArray[i,j];
+                    }
+                    if (floatArray[i, j] < min)
+                    {
+                        min = floatArray[i, j];
+                    }
+                }
+            }
+
+            return new Tuple<float, float>(min, max);
+        }
     }
 }
